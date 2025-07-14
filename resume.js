16e4a57fc08452818f34075f0f09af95cf733dec
Idham Malik <idham1432@gmail.com>
2025-07-15 03:33:49 +0800

    initial commit

---
+++ resume.js
@@ -0,0 +1,13 @@
+document.getElementById('btn-resume').addEventListener('click', function () {
+  const link = document.createElement('a');
+  link.href = 'Resume - Idham Malik.pdf';
+  link.download = 'Resume - Idham Malik.pdf';
+  link.click();
+});
+
+document.getElementById('btn-resume-sidebar').addEventListener('click', function () {
+  const link = document.createElement('a');
+  link.href = 'Resume - Idham Malik.pdf';
+  link.download = 'Resume - Idham Malik.pdf';
+  link.click();
+});
